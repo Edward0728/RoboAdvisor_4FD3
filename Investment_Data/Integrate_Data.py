185a3c69e4f3d6e210a9cb7e698735db1870ac14
Edward Yao <yaoh17@mcmaster.ca>
2022-10-12 21:18:37 -0400

    dummy for testing

--- Investment_Data/Integrate_Data.py
+++ Investment_Data/Integrate_Data.py
@@ -71,7 +71,6 @@ for i in symbol_list[0:-1]:
     #conn.request("GET", "/v1?url=https%3A%2F%2Fwww.theglobeandmail.com%2Finvesting%2Fmarkets%2Ffunds%2FTDB2766.CF%2Ffundamentals%2F&api_key=DBlmL7pZFYh6TNy71F8CR75fN0cPeT0y&device=desktop&proxy_type=datacenter")
     res = conn.getresponse()
     data = str(res.read())
-    #print(type(data))
     try:
         risk_index = data.index(find)
     except:
