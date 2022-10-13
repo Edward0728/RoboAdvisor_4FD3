17616208dc5295bc051d5ac8b0d665d393bad188
HuaYaoEdward <yaoh17@mcmaster.ca>
2022-10-12 21:03:59 -0400

    dummy change to test repo

--- Investment_Data/Get_Risk_One.py
+++ Investment_Data/Get_Risk_One.py
@@ -7,12 +7,6 @@ conn.request("GET", "/v1?url=https%3A%2F%2Fwww.theglobeandmail.com%2Finvesting%2
 res = conn.getresponse()
 data = str(res.read())
 print(type(data))
-#print(data.decode("utf-8"))
-
-# Html_file= open("C:/Users/forfu/source/repos/Edward0728/RoboAdvisor_4FD3/Investment_Data/RBF460-CF.html","w")
-# Html_file.write(str(data))
-# Html_file.close()
-#now =datetime.datetime.now()
 
 find = '<span class="black glyphicon glyphicon-chevron-up">'
 risk_index = data.index(find)
@@ -21,8 +15,4 @@ index_2 = data[risk_index-60:risk_index].index('<')
 print(index_1, index_2)
 print(data[risk_index-60:risk_index][index_1+1:index_2].strip())
 
-#url = 'http://www.basketball-reference.com/boxscores/index.cgi?lid=header_dateoutput&month={0}&day=17&year={2}'.format(cmonth,cday,cyear)
-#response = requests.get(url)
-# html = response.content
-# print(html)
 
