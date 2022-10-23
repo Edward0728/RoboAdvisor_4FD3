e8e7f96f527977904af1388e8f32130103e671e1
Edward Yao <forfutureyao@gmail.com>
2022-10-23 16:00:08 -0400

    solutions doc and data update

--- CSV_retriver.py
+++ CSV_retriver.py
@@ -2,8 +2,12 @@ import json
 
 import pandas as pd
 # Change the CSV file address to your CSV file path
-mutualFund_CSV = pd.read_csv (r'/Users/qinyang/PycharmProjects/RoboAdvisor/Investment_Data/all_funds_data.csv')
+#mutualFund_CSV = pd.read_csv (r'/Users/qinyang/PycharmProjects/RoboAdvisor/Investment_Data/all_funds_data.csv')
+mutualFund_CSV = pd.read_csv (r'C:/Users/forfu/source/repos/Edward0728/RoboAdvisor_4FD3/Investment_Data/all_funds_data.csv')
+risk_levels = ['None','Low','Low to Medium','Medium','Medium to High','High']
 
+symbol = 'TDB3491.CF'
+risk = risk_levels[2]
 
 def fundinfo_request(symbol):
     fund = symbol
@@ -11,10 +15,12 @@ def fundinfo_request(symbol):
     data = fund_info.to_json()
     return data
 
+#print(fundinfo_request(symbol))
 
-def fundlist_request(symbol):
-    risk_level = symbol
-    fund_list = mutualFund_CSV.loc[mutualFund_CSV['risk'] == risk_level, 'Symbol'].tolist()
+def riskfund_request(risk):
+    fund_list = mutualFund_CSV.loc[mutualFund_CSV['risk'] == risk, 'Symbol'].tolist()
     data = json.dumps(fund_list)
     return data
+
+#print(riskfund_request(risk))
 
