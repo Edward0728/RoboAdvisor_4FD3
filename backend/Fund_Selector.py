309723366b875438e050b98dcd04e6ac75995680
Edward Yao <forfutureyao@gmail.com>
2022-10-27 14:45:00 -0400

    update solution

--- backend/Fund_Selector.py
+++ backend/Fund_Selector.py
@@ -6,11 +6,11 @@ date = '2022-10-25'
 mutualFund_CSV = pd.read_csv (f'C:/Users/forfu/source/repos/Edward0728/RoboAdvisor_4FD3/Investment_Data/all_funds_all_data_{date}.csv')
 risk_levels = ['None','Low','Low to Medium','Medium','Medium to High','High']
 
-symbol = 'TDB3491.CF'
-risk = risk_levels[2]
-size = 'Medium'
-percentile = '30%'
-volatility = '0.2'
+# symbol = 'TDB3491.CF'
+# risk = risk_levels[2]
+# size = 'Medium'
+# percentile = '30%'
+# volatility = '0.2'
 
 def fundinfo_request(symbol):
     fund = symbol
