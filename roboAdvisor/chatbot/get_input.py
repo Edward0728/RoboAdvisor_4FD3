cfbbeee7c156f938c03deb48e56b9f2bcac0cf96
Edward Yao <forfutureyao@gmail.com>
2022-10-27 14:09:16 -0400

    update solution

--- roboAdvisor/chatbot/get_input.py
+++ roboAdvisor/chatbot/get_input.py
@@ -3,20 +3,23 @@
 import sqlite3
 
 #Create a connection to a database
-con=sqlite3.connect("db.sqlite3")
+con=sqlite3.connect("database.sqlite3")
 #con.close()
 #open cursor
 cursor=con.cursor()
 #Writing the query
-query="Select * from statement"
+query="select * from statement"
 #query= "SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';"
 #execute query
 cursor.execute(query)
 #fetch results
 result=cursor.fetchall()
 #view results
-for row in result:
-  print(row)
+
+with open('Cx_Input.txt', 'w') as output_file:
+    for row in result:
+        #print(str(row))
+        output_file.write(str(row)+'\n')
 #closing connection
 con.close
  
