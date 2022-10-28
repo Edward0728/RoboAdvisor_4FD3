05a85f61912a58f8c5347b4c52f04fb3461d579c
Edward Yao <forfutureyao@gmail.com>
2022-10-28 17:44:31 -0400

    user input update

--- roboAdvisor/chatbot/apps.py
+++ roboAdvisor/chatbot/apps.py
@@ -2,7 +2,16 @@ from django.apps import AppConfig
 from chatbot import chatbot
 from flask import Flask, render_template, request
 from chatterbot.conversation import Statement
+from datetime import datetime
+ 
+def dt_name():
+    dt_obj = datetime.now()
+    
+    #date and time in a different format :# dd/mm/YY H:M:S
+    
+    return dt_obj.strftime("%m%d%Y%H%M%S")
 
+dt = dt_name()
 
 class ChatbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -21,7 +30,7 @@ def get_bot_response():
     userText = request.args.get('msg')
     input_statement = Statement(text=userText)
     response = str(chatbot.generate_response(input_statement))
-    with open('conversation.txt', 'a') as output_file:
+    with open(f'{dt}.txt', 'a') as output_file:
       #print(str(row))
       output_file.write(str(input_statement)+'\n')
       output_file.write(response+'\n')
