49f45d50467d51d014b8cc559cc487bef0949a47
qinyang <qinyang789@gmail.com>
2022-11-14 22:25:07 -0500

    Stock Frontend Backend Interface Development

---
+++ LSTM.py
@@ -0,0 +1,35 @@
+# Package Plan
+# import numpy as np
+# import pandas as pd
+# import os
+# import gc
+# import copy
+# import warnings
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import plotly.graph_objects as go
+# import plotly.express as px
+# from plotly.subplots import make_subplots
+# from IPython.display import display_html
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense
+# from tensorflow.keras.layers import LSTM
+# from keras.layers import Dropout
+# from tensorflow import keras
+
+
+# LSTM Game Plan
+# 1. Acquire Stock Data Using API
+# 2. Visualize Historical Stock Price
+# 3. Prepare Training Data Set
+# 4. Prepare Test Data Set
+# 5. Setup LSTM Network Architecture
+# 6. Train LSTM Model
+# 7. Evaluate Model with RMSE
+# 8. Visualize Predicted Price
+
+
+print("LSTM")
+
